081e17a14eb477a7837be62b185389009131efc8
BigRockerEnding <bigrockerending@live.com>
2015-11-21 11:12:20 -0800

    drum kit finished
    sdfs

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -17,7 +17,9 @@ import javax.swing.JPanel;
 
 public class DrumKit extends MouseAdapter {
 
-	JLabel hello;
+	JLabel bassLabelImage;
+	JLabel snareLabelImage;
+	JLabel cowLabelImage;
 
 	public static void main(String[] args) throws Exception {
 		new DrumKit().getGoing();
@@ -25,41 +27,50 @@ public class DrumKit extends MouseAdapter {
 
 	private void getGoing() throws MalformedURLException {
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
-		JFrame yo = new JFrame();
+		JFrame yoFrame = new JFrame();
 		// 2. Make the frame visible
-		yo.setVisible(true);
+		yoFrame.setVisible(true);
 		// 3. Set the size of the frame
-		yo.setSize(500, 500);
+		yoFrame.setSize(5000, 5000);
+		yoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 4. Set the title of the frame
-		yo.setTitle("drums");
+		yoFrame.setTitle("drums");
 		// 5. Make a JPanel and initialize it.
-		JPanel hi = new JPanel();
-		
-		 JPanel panel = new JPanel();
+		JPanel bassPanel = new JPanel();
+		JPanel snarePanel = new JPanel();
+		JPanel cowPanel = new JPanel();
+
 		// 6. Add the panel to the frame. (The panel is invisible.)
-		yo.add(hi);
-		yo.add(panel);
+		yoFrame.add(bassPanel);
+		yoFrame.add(snarePanel);
+		yoFrame.add(cowPanel);
+
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
 		// 8. Put the name of your image in a String variable.
+
 		String bass = "398818RXBF.jpg";
 		String snare = "MC_PP130.jpg";
+		String cow = "maxresdefault.jpg";
 		// 9. Use the "createLabelImage()" method to initialize the
 		// drumLabelWithImage variable.
-		hello = createLabelImage(bass);
-		hello = createLabelImage(snare);
+		bassLabelImage = createLabelImage(bass);
+		snareLabelImage = createLabelImage(snare);
+		cowLabelImage = createLabelImage(cow);
 		// 10. Add the image to the panel
-		hi.add(hello);
-		panel.add(hello);
+		snarePanel.add(snareLabelImage);
+		bassPanel.add(bassLabelImage);
+		cowPanel.add(cowLabelImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-		hi.setLayout(new GridLayout());
-		panel.setLayout(new GridLayout());
+		yoFrame.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame
-		yo.pack();
-		yo.pack();
+		yoFrame.pack();
 
 		// 13. add a mouse listener to drumLabelWithImage.
-		hello.addMouseListener(this);
+		snareLabelImage.addMouseListener(this);
+		bassLabelImage.addMouseListener(this);
+		cowLabelImage.addMouseListener(this);
+
 	}
 
 	public void mouseClicked(MouseEvent e) {
@@ -70,10 +81,22 @@ public class DrumKit extends MouseAdapter {
 		// There are some sounds here: http://bit.ly/drum-sounds or you can also
 		// get them from freesound.org
 		// 16. If they clicked on the drumImage...
+
 		String bass = "103362__dmlandrum__dml-bass-drum (2).wav";
-		if (hello.equals(drumClicked)) {
+		if (bassLabelImage.equals(drumClicked)) {
 			playSound(bass);
 		}
+
+		String snare = "270156__theriavirra__04c-snare-smooth-cymbals-snares.wav";
+		if (snareLabelImage.equals(drumClicked)) {
+			playSound(snare);
+		}
+		
+		String cow = "58277__benboncan__cow.wav";
+		if (cowLabelImage.equals(drumClicked)) {
+			playSound(cow);
+		}
+
 		// 17. ...use the playSound method to play a drum sound.
 
 		// 18. Add more images to make a drumkit. Remember to add a mouse
