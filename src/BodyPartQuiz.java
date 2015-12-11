a898b41e87a71be74d607dc9e3cad8fbcf321d88
Dave <dave.dunn457@gmail.com>
2015-12-10 19:54:58 -0800

    Simon Says and Celebrity Game
    simon says = unfinished
    celeb game = finished

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -12,20 +12,55 @@ public class BodyPartQuiz {
 
 	// Get 3 celebrity photos from the internet, save them to your computer, and fill
 	// in their paths here. To get the path, right click on the image, and copy its Location.
-	String taylorImage = "22fe4638-d675-47f8-9726-5f43e27bb084.jpeg";
-	String selenaImage = "MTE5NDg0MDU0ODM5Mzk1ODU1.jpg";
-	String adeleImage = "adele-25-packshot.jpg";
+	String taylorImage = "/Users/league/Documents/ShivaLevel1/src/22fe4638-d675-47f8-9726-5f43e27bb084.jpeg";
+	String selenaImage = "/Users/league/Documents/ShivaLevel1/src/MTE5NDg0MDU0ODM5Mzk1ODU1.jpg";
+	String adeleImage = "/Users/league/Documents/ShivaLevel1/src/adele-25-packshot.jpg";
 
 	Frame window = new Frame();
 	
 	private void startQuiz() {
 		// Set the size of the window so that you can only see part of the image.
-			window.setSize(500,500);
+			window.setSize(300, 300);
 		// Make an int variable to hold the score.
 			int score = 0;
 		// Ask the user who this person is and store their answer
 		String taylorAnswer = JOptionPane.showInputDialog("Who is this celebrity?");
 		// If they got the answer right:
+		if (taylorAnswer.equals("Taylor Swift")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "CORRECT!! Moving On . . .");
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT!! Moving On . . .");
+		}
+		
+		showNextImage();
+		window.setSize(300,300);
+		String selenaAnswer = JOptionPane.showInputDialog("Who is this celebrity?");
+		if (selenaAnswer.equals("Selena Gomez")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "CORRECT!! Moving On . . .");
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT!! Moving On . . .");
+		}
+		
+		showNextImage();
+		window.setSize(300, 400);
+		String adeleAnswer = JOptionPane.showInputDialog("Who is this celebrity?");
+		if (adeleAnswer.equals("Adele")) {
+			 score++;
+			JOptionPane.showMessageDialog(null, "CORRECT!! Moving On . . .");
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT!! ");
+		}
+		
+		JOptionPane.showMessageDialog(null, "Your score is " + score++);
+		
 		// -- Tell them they are correct.
 		// -- Increase the score by 1
 
