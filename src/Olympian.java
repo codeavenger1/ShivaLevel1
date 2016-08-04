6c28f8bad8adbf4567fd02ed92e5bd079bae1e71
TJHL <league@WTS5.attlocal.net>
2016-08-03 20:04:31 -0700

    Makey Makey 2 and Fantasy Olympics
    Makes Makey —> finished
    Fantasy Olympics —> unfinished

---
+++ src/Olympian.java
@@ -0,0 +1,20 @@
+
+public class Olympian {
+
+String name;
+int numberOfMedals;
+String typeOfSport;
+Boolean girl;
+int Age;
+String country;
+
+public Olympian(String name, int numberOfMedals,String typeOfSport, Boolean girl, int Age, String country){
+	this.name = name;
+	this.numberOfMedals = numberOfMedals;
+	this.typeOfSport = typeOfSport;
+	this.girl = girl;
+	this.Age = Age;
+	this.country = country;
+	
+}
+}
---
+++ src/OlympicTeam.java
@@ -0,0 +1,6 @@
+
+public class OlympicTeam {
+public static void main(String[] args) {
+	Olympian Missy = new Olympian("Missy Franklin", 0, "Compettitive Swimming", true, 21, "USA");
+}
+}
