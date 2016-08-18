1e929599f41bc4b2df786ba2e91ccfb621d74dbc
Dong Zou <jiwenzou@gmail.com>
2016-08-17 19:57:29 -0700

    Olympian, Spelling Bee
    finished!
    finished!

--- src/Olympian.java
+++ src/Olympian.java
@@ -1,20 +1,29 @@
 
 public class Olympian {
 
-String name;
-int numberOfMedals;
-String typeOfSport;
-Boolean girl;
-int Age;
-String country;
+	int bibNumber;
+	static int number = 0;
+	String name;
+	int numberOfGoldMedals;
+	String typeOfSport;
+	Boolean girl;
+	int Age;
+	String country;
 
-public Olympian(String name, int numberOfMedals,String typeOfSport, Boolean girl, int Age, String country){
-	this.name = name;
-	this.numberOfMedals = numberOfMedals;
-	this.typeOfSport = typeOfSport;
-	this.girl = girl;
-	this.Age = Age;
-	this.country = country;
-	
-}
+	public Olympian(String name, int numberOfMedals, String typeOfSport, Boolean girl, int Age, String country) {
+		this.name = name;
+		this.numberOfGoldMedals = numberOfMedals;
+		this.typeOfSport = typeOfSport;
+		this.girl = girl;
+		this.Age = Age;
+		this.country = country;
+		number++;
+		bibNumber = number;
+	}
+
+	void Perform() {
+		System.out.println(
+				"#" + bibNumber + " " + name + " - " + name + " had come to the Olympics to compete in " + typeOfSport
+						+ ", " + "has " + numberOfGoldMedals + " gold medal, and is " + Age + ". Go " + country + "!");
+	}
 }
--- src/OlympicTeam.java
+++ src/OlympicTeam.java
@@ -1,6 +1,16 @@
 
 public class OlympicTeam {
-public static void main(String[] args) {
-	Olympian Missy = new Olympian("Missy Franklin", 0, "Compettitive Swimming", true, 21, "USA");
-}
+	public static void main(String[] args) {
+		Olympian Franklin = new Olympian("Missy Franklin", 1, "Swimming", true, 21, "USA");
+		Olympian Ledecky = new Olympian("Katie Ledecky", 4, "Swimming", true, 19, "USA");
+		Olympian King = new Olympian("Lily King", 0, "Swimming", true, 19, "USA");
+		Olympian Schmitt = new Olympian("Allison Schmitt", 4, "Swimming", true, 26, "USA");
+		Olympian Ervin = new Olympian("Anthony Ervin", 35, "Swimming", false, 2, "USA");
+
+		Franklin.Perform();
+		Ledecky.Perform();
+		King.Perform();
+		Schmitt.Perform();
+		Ervin.Perform();
+	}
 }
